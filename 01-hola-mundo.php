9b6fc86ed0ffdf6257b146c02bdb1f6d9d45f8b5
luiarmandoaguayoaceves <luisarmandoaguayoaceves@gmail.com>
2022-08-06 09:20:45 -0500

    primer comit de php

---
+++ 01-hola-mundo.php
@@ -0,0 +1,8 @@
+<?php include 'includes/header.php';
+
+echo "Maneras de imprimir en pantalla con echo<br>";
+print "con print<br>";
+print_r ("printi_r con parentesis <br>");
+var_dump("var_dump con aparentesis y da informacion del dato");
+
+include 'includes/footer.php';
