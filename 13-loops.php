8e662f47c250f33a8087eb289e1aab8bc2dbf661
Luis Armando AA <luisarmandoaguayoaceves@gmail.com>
2023-03-01 09:18:41 -0600

    actualizar php comit completo

--- 13-loops.php
+++ 13-loops.php
@@ -5,7 +5,17 @@
 //do While
 
 //for loop
-
+for ($i=1; $i < 1000; $i++) { 
+    if ($i % 3 ===0 && $i % 5 === 0) {
+        echo $i . "-FIZZ BUZZ </br>";
+    }else if ($i % 3 === 0){
+        echo $i . "-Fizz </br>";
+    }else if ($i % 5 === 0){
+        echo $i . "-Buzz </br>";
+    }else {
+        echo $i . "</br>";
+    }
+}
 
 
 include 'includes/footer.php';
