ecd06df88b37ec0a3df87db95538f8f2c65f178a
Luis Armando AA <luisarmandoaguayoaceves@gmail.com>
2023-03-01 11:31:06 -0600

    finish loops

--- 13-loops.php
+++ 13-loops.php
@@ -1,18 +1,27 @@
 <?php include 'includes/header.php';
 
 //While
-
+$i = 0;
+while($i < 10) {
+    echo $i . "</br>";
+    
+    $i++;
+}
 //do While
 
+do {
+    echo $i . "</br>";
+} while ($i < 10);
+
 //for loop
-for ($i=1; $i < 1000; $i++) { 
-    if ($i % 3 ===0 && $i % 5 === 0) {
+for ($i = 1; $i < 1000; $i++) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
         echo $i . "-FIZZ BUZZ </br>";
-    }else if ($i % 3 === 0){
+    } else if ($i % 3 === 0) {
         echo $i . "-Fizz </br>";
-    }else if ($i % 5 === 0){
+    } else if ($i % 5 === 0) {
         echo $i . "-Buzz </br>";
-    }else {
+    } else {
         echo $i . "</br>";
     }
 }
