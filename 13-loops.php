b47184e402fbc597c1d556def3b08575f722b053
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2023-03-05 14:05:21 -0600

    foreach

--- 13-loops.php
+++ 13-loops.php
@@ -1,30 +1,54 @@
 <?php include 'includes/header.php';
-
+                                        /* Se puede usar las llaver para teminar ordenes pero tambien
+                                         se puede usar la palabra end seguido del tipo de instruccion
+                                          por ejemplo if(): endif, for(): endfor, while: endwhile*/ 
 //While
 $i = 0;
-while($i < 10) {
-    echo $i . "</br>";
+// while($i < 10) {
+//     echo $i . "</br>";
     
-    $i++;
-}
-//do While
+//     $i++;
+// }
+// //do While
 
-do {
-    echo $i . "</br>";
-} while ($i < 10);
+// do {
+//     echo $i . "</br>";
+// } while ($i <br 10);
 
-//for loop
-for ($i = 1; $i < 1000; $i++) {
-    if ($i % 3 === 0 && $i % 5 === 0) {
-        echo $i . "-FIZZ BUZZ </br>";
-    } else if ($i % 3 === 0) {
-        echo $i . "-Fizz </br>";
-    } else if ($i % 5 === 0) {
-        echo $i . "-Buzz </br>";
-    } else {
-        echo $i . "</br>";
-    }
-}
+// //for loop
+// for ($i = 1; $i <1000; $i++):
+//     if ($i % 3 === 0 && $i % 5 === 0) :
+//         echo $i . "-FIZZ BUZZ </br>";
+//     elseif ($i % 3 === 0) :
+//         echo $i . "-Fizz </br>";
+//     elseif ($i % 5 === 0) :
+//         echo $i . "-Buzz </br>";
+//     else :
+//         echo $i . "</br>";
+//     endif;
+// endfor;
 
+// For Each
+$clientes = array('pedro', 'Juan', 'Karen');
+foreach($clientes as $cliente) :
+    echo $cliente . "</br>";
+endforeach;
+
+// echo count($clientes)."</br>";
+// echo sizeof($clientes);
+
+for ($i = 0; $i < count($clientes); $i++):
+    echo $clientes[$i]."</br>";
+endfor;
+
+$cliente = [
+    'nombre' => 'juan',
+    'saldo' => 200,
+    'tipo' => 'premium'
+];
+
+foreach ($cliente as $key => $valor):
+    echo $key . " - ",$valor . "</br>";
+endforeach;
 
 include 'includes/footer.php';
--- 14-foreach.php
+++ 14-foreach.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'TV 40"',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Celular',
+        'precio' => 400,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Tablet',
+        'precio' => 100,
+        'disponible' => false
+    ]
+];
 
-
+foreach ($productos as $producto):?>
+<li>
+    <p> Nombre producto: <?php echo $producto['nombre'] ?> </p>
+    <p> Precio: <?php echo $producto['precio'] ?> </p>
+    <p> <?php echo ($producto['disponible']) ? 'Disponible' : 'No disponible'; ?> </p> <!-- Ternario y un if es similar -->
+</li>
+<?php
+endforeach;
 
 include 'includes/footer.php';
