a76b183ecdd261fef8c9700c2872dd10563cdcef
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2023-03-05 14:17:01 -0600

    funciones

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,6 +1,16 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1);//obliga a poner a fuersa int's
+include 'includes/header.php';
 
+function sumar(int $num1 = 0, int $num2 = 0){
+    echo $num1+$num2;
+}
 
+sumar(10);
+echo "</br>";
+sumar(10,15);
+echo "</br>";
+//Error sumar(10,'hola');
 
 
 
