4393b49cd8ab54eb051e02ae0359ecbe624401bb
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2023-03-05 15:01:30 -0600

    el uso de los templates o requieres

--- 17-require.php
+++ 17-require.php
@@ -1,4 +1,11 @@
-<?php include 'includes/header.php';
+<?php require 'includes/header.php';
+//requiere cuando tengamos funciones criticas a un servidor
+//include cuando se pueda o no poner ese codigo para cosas mas como opcionales
+//requiere_once lo incluye pero valida si ya fue llamado antes
+require 'funciones.php';
+
+agregarApp();
+
 
 
 
---
+++ funciones.php
@@ -0,0 +1,5 @@
+<?php
+
+function agregarApp(){
+    echo "Agregamos App...";
+}
