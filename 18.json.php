aaa02722ca177ee5fef68a7d1441cc4afc6c909d
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2023-03-05 15:13:37 -0600

    fin de conseptos basicos en php json ultimo tema

--- 18.json.php
+++ 18.json.php
@@ -1,7 +1,30 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'TV 40',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Celular',
+        'precio' => 400,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Tablet',
+        'precio' => 100,
+        'disponible' => false
+    ]
+];
 
-
+echo "<pre>";
+var_dump($productos);
+$json = json_encode($productos);
+$json_array = json_decode($json);
+var_dump($json);
+var_dump($json_array);
+echo "</pre>";
 
 
 
